69043651ee87e60169bc10ac6dc9201a0d83423a
Darkov <darkov99999@gmail.com>
2020-01-23 14:08:59 +0200

    first commit

---
+++ TestService/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TestService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<XElement> Test()
+        {
+            var rng = new Random();
+            return XElement.Parse(@"<test>" + rng.Next(0, 101) + @"</test>");
+        }
+    }
+}
